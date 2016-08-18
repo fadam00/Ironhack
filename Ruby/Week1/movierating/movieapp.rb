541c640bce1f9c308aa35a8604dec0597792b3ad
Frank Adames <frankv.adames@outlook.com>
2016-08-18 09:43:26 -0400

    Final version of movie rating project

--- Ruby/Week1/movierating/movieapp.rb
+++ Ruby/Week1/movierating/movieapp.rb
@@ -1,39 +1,68 @@
 require("imdb")
 
 
-	text = IO.readlines("movies.txt")
-	movies = []
-	grid = ""
+    text = IO.readlines("movies.txt")
+    movies = []
+    grid = ""
+    #     text.each do |x|
+    #     the_search = Imdb::Search.new(x)
 
-	text.each do |x|
-		the_search = Imdb::Search.new(x)
+
+    #     first_result = the_search.movies[0]
+    #     a = first_result.title
+    #     b =first_result.rating.to_i
+
+    #     moviehash = {:name => a, :rating => b}
+    #     movie.push (moviehash)
+
+    #     end
+
+    list = ""
+    numb = 1
+    text.each do |x|
+        the_search = Imdb::Search.new(x)
 
 
-		first_result = the_search.movies[0]
-		a = first_result.title
-		b =first_result.rating
+        first_result = the_search.movies[0]
+        a = first_result.title
+        b =first_result.rating
 
-		moviehash = {:name => a, :rating => b}
-		movies.push (moviehash)
-		end
+        list += "#{numb}. #{first_result.title} \n"
+        numb += 1
+        
+        moviehash = {:name => a, :rating => b}
+        movies.push (moviehash)
+        end
 
-puts "Ratings"
-
+puts "      Ratings"
+l = 1
 10.downto(1).each do |rating|
-	movies.each do |movie|
-		
-		 if movie[:rating] >= rating then
-		 	grid += "|#|"
+    movies.each do |movie|
+        
+         if movie[:rating] >= rating then
+             grid += "|#|"
 
-		 else 
-		 	grid += "|_|"
-		 	
-		 end
-	end
-	grid += "\n"
+         else 
+             grid += "| |"
+             
+         end
+    end
+    grid += "\n"
 end
 
-puts grid
+puts grid 
+puts " ____________________"
+print " 1  2  3  4  5  6  7\n\n"
+
+puts list
+
+
+
+
+
+
+
+
 
 
 
