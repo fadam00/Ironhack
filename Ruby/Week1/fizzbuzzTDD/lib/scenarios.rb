2b5403321867d60b9750603b84229897231af586
Frank Adames <frankv.adames@outlook.com>
2016-08-22 14:47:04 -0400

    FizzBuzzTDD Project Ver 1

---
+++ Ruby/Week1/fizzbuzzTDD/lib/scenarios.rb
@@ -0,0 +1,17 @@
+class FizzBuzzy
+	def fizz_buzz(number)
+		if number % 3 == 0 && number % 5 == 0
+			"FizzBuzz"
+		elsif number % 3 == 0
+			 "Fizz"
+
+		elsif number % 5 == 0
+			"Fuzz"
+		else
+			number
+		end
+	end
+end
+
+			
+
