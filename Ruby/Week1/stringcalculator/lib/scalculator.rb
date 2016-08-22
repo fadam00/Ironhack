49f486c42b7724eed283398f5e4bc31e02c0d983
Frank Adames <frankv.adames@outlook.com>
2016-08-22 11:29:18 -0400

    Added commentss

--- Ruby/Week1/stringcalculator/lib/scalculator.rb
+++ Ruby/Week1/stringcalculator/lib/scalculator.rb
@@ -4,14 +4,15 @@ class StringCalculator
 		@string_to_int = string.gsub(/\$/, ",")
 		@string_to_int1 = @string_to_int.split()
 
-		#@string_to_int[0].to_i + @string_to_int[1].to_i
+
+#This works (except for bonus) ==>	#@string_to_int[0].to_i + @string_to_int[1].to_i
+
+#The loop isn't working. Check again and also check instace variables.
 		sum = 0
 		@string_to_int1.each do |x|
 			x.to_i
 			sum += x
 			end
-
-
 
 	end
 end
