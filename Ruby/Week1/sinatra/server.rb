a771d224596cb67ceafb6d293ad88fc8ef201b89
Frank Adames <frankv.adames@outlook.com>
2016-08-23 14:59:06 -0400

    First version sinatra project

---
+++ Ruby/Week1/sinatra/server.rb
@@ -0,0 +1,31 @@
+require "sinatra"
+
+get "/" do
+	erb :home
+	end
+
+get "/company" do
+	erb :company
+	end
+get "/about" do
+	# Refers to views/about.erb
+	erb :about
+end
+
+users = [
+	{:username => "starwars", :name => "Star Wars", :bio => "In a galaxy far far away", :avatar => "/chukkachukka.jpg"}
+]
+
+get "/users/:username" do
+	
+	@user_name_string = params[:username]
+
+	@the_user = users.find {|the_user|
+		the_user[:username] == @user_name_string}
+
+	if @the_user == nil
+		erb :no_user
+	else
+		erb :user_profile
+	end
+end
