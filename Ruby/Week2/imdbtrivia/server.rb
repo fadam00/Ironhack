1b7df255d47082bb1c116c6f6a3ade03cbfbec9c
Frank Adames <frankv.adames@outlook.com>
2016-08-30 09:20:23 -0400

    90% DONE, still facing problems with prompting user to answer question.

--- Ruby/Week2/imdbtrivia/server.rb
+++ Ruby/Week2/imdbtrivia/server.rb
@@ -10,9 +10,16 @@ end
 post "/search_results" do
   the_search = Imdb::Search.new(params[:word])
 
-  @movies = the_search.movies.first(30)
+  index = 0
+
+
+  @movies = the_search.movies.first(30)#or you can use .sample to grab a single random item from the array.
   @movies.delete_if{|movie| movie.poster == nil}
   @movies_with_posters = @movies.first(9)
+
+  def movie_question(response) do
+    	if @response = movies_with_posters[0].year
+
 
 
   erb :search_results
