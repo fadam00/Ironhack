f20d4201ed24ad22e8eae08fd102d303ffaed240
Frank Adames <frankv.adames@outlook.com>
2016-09-07 12:25:43 -0400

    Added projects route to TimeTracking app

--- Ruby/Week4/timetracking/config/routes.rb
+++ Ruby/Week4/timetracking/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-	get '/' , to: 'site#home'
+	get '/' , to: 'site#home', as: :home
 	# get ("/", {to: "site#home"}) <---Hash
-	get '/contact', to: 'site#contact'
+	get '/contact', to: 'site#contact', as: :contact
 
-	get '/say_name/:name', to: 'site#say_name'
+	get '/say_name/:name', to: 'site#say_name', as: :name
+
+	get '/projects', to: 'projects#index', as: :projects
 end
