2450de492c902a8a343306cfd055dbe7969f6f82
Frank Adames <frankv.adames@outlook.com>
2016-09-13 15:37:46 -0400

    Added price modification

--- Javascript/Week5/jQuery/pizza-builder-master/src/pizza.js
+++ Javascript/Week5/jQuery/pizza-builder-master/src/pizza.js
@@ -1,26 +1,48 @@
 // Write your Pizza Builder JavaScript in this file.
-$('.btn-pepperonni').on('click', function(){
-	$('.pep').toggle();
-		$('.btn-pepperonni').toggleClass('active');
+$(document).ready(function(){
+
+var price = 10
+// $('.total').html("$" + price);
+
+$(".btn").on('click', function(){
+	$('.total').html("$" + price);
 });
 
-$('.btn-mushrooms').on('click', function(){
-	$('.mushroom').toggle();
-		$('.btn-mushrooms').toggleClass('active');
-});
+	$('.btn-pepperonni').on('click', function(){
+		$('.pep').toggle();
+			if ($('.btn-pepperonni').hasClass('active')) {	
+				 price += 1;
+				 
+		}	else{
+				price -= 1;
+		};		
+				$('.btn-pepperonni').toggleClass('active')
+	});
 
-$('.btn-green-peppers').on('click', function(){
-	$('.green-pepper').toggle();
-		$('.btn-green-peppers').toggleClass('active');
-});
 
-$('.btn-sauce').on('click', function(){
-	$('.sauce').toggleClass('sauce-white');
-		$('.btn-sauce').toggleClass('active');
-});
+	$('.btn-mushrooms').on('click', function(){
+		$('.mushroom').toggle();
+			$('.btn-mushrooms').toggleClass('active');
+	});
 
-$('.btn-crust').on('click', function(){
-	$('.crust').toggleClass('crust-gluten-free');
-		$('.btn-crust').toggleClass('active');
+	$('.btn-green-peppers').on('click', function(){
+		$('.green-pepper').toggle();
+			$('.btn-green-peppers').toggleClass('active');
+	});
+
+	$('.btn-sauce').on('click', function(){
+		$('.sauce').toggleClass('sauce-white');
+			$('.btn-sauce').toggleClass('active');
+	});
+
+	$('.btn-crust').on('click', function(){
+		$('.crust').toggleClass('crust-gluten-free');
+			$('.btn-crust').toggleClass('active');
+	});
+//When pepperonni button is clicked $1 is added to price ($10) and changes price to $11.
+
+// var price = 10
+$('.total').html("$" + price);
+
 });
 
