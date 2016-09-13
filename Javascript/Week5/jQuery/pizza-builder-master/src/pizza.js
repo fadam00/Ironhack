9df50ad86cb8fbf4c67fda38c2367e62a6f06996
Frank Adames <frankv.adames@outlook.com>
2016-09-13 14:27:49 -0400

    Added activate/deactivate

--- Javascript/Week5/jQuery/pizza-builder-master/src/pizza.js
+++ Javascript/Week5/jQuery/pizza-builder-master/src/pizza.js
@@ -1,23 +1,26 @@
 // Write your Pizza Builder JavaScript in this file.
 $('.btn-pepperonni').on('click', function(){
 	$('.pep').toggle();
-	$('.pep').removeClass('active');
-	$('.btn-pepperonni').toggleClass('active');
+		$('.btn-pepperonni').toggleClass('active');
 });
 
 $('.btn-mushrooms').on('click', function(){
 	$('.mushroom').toggle();
+		$('.btn-mushrooms').toggleClass('active');
 });
 
 $('.btn-green-peppers').on('click', function(){
 	$('.green-pepper').toggle();
+		$('.btn-pepperonni').toggleClass('active');
 });
 
 $('.btn-sauce').on('click', function(){
 	$('.sauce').toggleClass('sauce-white');
+		$('.btn-pepperonni').toggleClass('active');
 });
 
 $('.btn-crust').on('click', function(){
 	$('.crust').toggleClass('crust-gluten-free');
+		$('.btn-pepperonni').toggleClass('active');
 });
 
