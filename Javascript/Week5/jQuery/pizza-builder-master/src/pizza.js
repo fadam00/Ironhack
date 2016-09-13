cbb39aa20179028491a8d4f3fee4d9e3af234bda
Frank Adames <frankv.adames@outlook.com>
2016-09-13 14:04:03 -0400

    Added Hide/Show to mushrooms

--- Javascript/Week5/jQuery/pizza-builder-master/src/pizza.js
+++ Javascript/Week5/jQuery/pizza-builder-master/src/pizza.js
@@ -2,3 +2,15 @@
 $('.btn-pepperonni').on('click', function(){
 	$('.pep').toggle();
 });
+
+$('.btn-mushrooms').on('click', function(){
+	$('.mushroom').toggle();
+});
+
+$('.btn-pepperonni').on('click', function(){
+	$('.pep').toggle();
+});
+
+$('.btn-pepperonni').on('click', function(){
+	$('.pep').toggle();
+});
