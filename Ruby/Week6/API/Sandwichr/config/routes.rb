19bd6494fdae06c3eaef033b330b25e38de934c2
Frank Adames <frankv.adames@outlook.com>
2016-09-19 16:15:12 -0400

    Made it so POST could be done on Postman

--- Ruby/Week6/API/Sandwichr/config/routes.rb
+++ Ruby/Week6/API/Sandwichr/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :ingredients, except: [:new, :edit]
   resources :sandwiches, except: [:new, :edit]
 
-  post "/sandwiches/:id/ingredients/add/:ingredient_id", to: "sandwiches#add", as: :add_ingredients
+  post "/sandwiches/:id/ingredients/add", to: "sandwiches#add", as: :add_ingredients
   
 end
